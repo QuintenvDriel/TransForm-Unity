5fc478ea5a23d3767b797ef267d404863172d377
Quinten vD <89836942+QuintenvDriel@users.noreply.github.com>
2025-05-14 13:18:55 +0200

    Done
    Startscene klaar.

---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+
+    public Image fadeImage;
+    public float fadeDuration = 1f;
+
+    public void StartGame()
+    {
+        StartCoroutine(FadeOutAndLoadScene("HomeScene"));
+    }
+
+    private IEnumerator FadeOutAndLoadScene(string sceneName)
+    {
+        fadeImage.color = new Color(0, 0, 0, 0);
+
+        fadeImage.gameObject.SetActive(true);
+
+        //Fading naar zwart
+        float timeElapsed = 0f;
+        while (timeElapsed < fadeDuration)
+        {
+            timeElapsed += Time.deltaTime;
+            fadeImage.color = new Color(0, 0, 0, Mathf.Clamp01(timeElapsed / fadeDuration));
+            yield return null;
+        }
+
+        // Laad de nieuwe scéne en wacht tot deze volledig is geladen
+        AsyncOperation asyncload = SceneManager.LoadSceneAsync(sceneName);
+
+        while (!asyncload.isDone)
+        {
+            yield return null;
+        }
+
+        //Wacht zodat het fade effect goed te zien is
+        timeElapsed = 0f;
+        while (timeElapsed < fadeDuration)
+        {
+            timeElapsed += Time.deltaTime;
+            fadeImage.color = new Color(0, 0, 0, Mathf.Clamp01(timeElapsed / fadeDuration));
+            yield return null;
+        }
+    }
+
+}
+
